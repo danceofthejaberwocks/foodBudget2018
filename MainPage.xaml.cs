2064a03c67e47fa008f7db0e4993c1f75cf4b8a8
danceofthejaberwocks <Suzanne.smith928@gmail.com>
2018-09-06 15:01:53 -0500

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace foodBudget2018
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class recipeAdd : ContentPage
+	public partial class MainPage : ContentPage
 	{
-		public recipeAdd ()
-        {
-            new recipeAdd();
-        }
+		public MainPage()
+		{
 
-           
-           
-		}
-	}
+            InitializeComponent();
+            new MainPage();		}
+
+       /* when you click the Add Recipe button; this returns the Add Recipe view  */
+        async void recipeAdd(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new recipeAdd());
+                }
+       
+    }
+}
